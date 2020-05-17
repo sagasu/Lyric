2054afa4f0d22c1f9489e0e1a8ce65e82b3a1866
Mateusz Kopij <sagasu@gmail.com>
2020-05-17 19:59:59 +0100

    introducing song list component

---
+++ client/components/song-list.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo';
+
+class SongList extends Component{
+
+    renderSongs() {
+        return this.props.data.songs.map(song => {
+            return (
+                <li>
+                    {song.title}
+                </li>
+            );
+        });
+    }
+
+    render() {
+        console.log(this.props);
+        
+        if (this.props.data.loading) {
+            return <div>Loading</div>
+        }
+
+        return(
+            <div>
+                {this.renderSongs()}
+            </div>
+        );
+    }
+}
+
+const query = gql`
+{
+    songs {
+        title
+    }
+}
+`;
+
+export default graphql(query)(SongList);
--- client/index.js
+++ client/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import {ApolloProvider} from 'react-apollo';
+import SongList from './components/song-list';
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return(
     <ApolloProvider client={client}>
-     <div>Lyric</div>
+      <SongList></SongList>
     </ApolloProvider>
   );
 };
